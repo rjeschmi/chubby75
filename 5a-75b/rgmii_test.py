9f9e3c28f9421eda3148af52902e5a3d6314b400
Robert Schmidt <rjeschmi@gmail.com>
2020-03-19 16:07:00 -0400

    now with more pings

--- 5a-75b/rgmii_test.py
+++ 5a-75b/rgmii_test.py
@@ -6,6 +6,7 @@ from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.soc.cores.clock import *
 
+from liteeth.common import convert_ip
 from liteeth.phy.ecp5rgmii import LiteEthPHYRGMII
 from liteeth.core import LiteEthUDPIPCore
 from liteeth.frontend.etherbone import LiteEthEtherbone
@@ -14,7 +15,8 @@ from litex_boards.platforms import colorlight_5a_75b
 
 # CRG ----------------------------------------------------------------------------------------------
 
-class _CRG(Module):
+class RGMIITestCRG(Module):
+    '''A class to define the clock and reset Generator'''
     def __init__(self, platform, sys_clk_freq):
         self.clock_domains.cd_sys    = ClockDomain()
         self.clock_domains.cd_sys_ps = ClockDomain()
@@ -30,15 +32,16 @@ class _CRG(Module):
 
 # RGMIITest ----------------------------------------------------------------------------------------
 
-class RGMIITest(SoCMini):
-    def __init__(self, platform, eth_phy=0, mac_address=0x10e2d5000000, ip_address="192.168.1.50"):
+class RGMIITestSoC(SoCMini):
+    '''Implement the SoC side of the test'''
+    def __init__(self, platform, eth_phy=0, mac_address=0x10e2d5000000, ip_address="192.168.88.50"):
         sys_clk_freq = int(133e6)
 
         # SoCMini ----------------------------------------------------------------------------------
         SoCMini.__init__(self, platform, sys_clk_freq)
 
         # CRG --------------------------------------------------------------------------------------
-        self.submodules.crg = crg = _CRG(platform, sys_clk_freq)
+        self.submodules.crg = crg = RGMIITestCRG(platform, sys_clk_freq)
 
         # 1 Gbps Ethernet --------------------------------------------------------------------------
         # phy
@@ -49,12 +52,15 @@ class RGMIITest(SoCMini):
         ethcore = LiteEthUDPIPCore(
             phy         = ethphy,
             mac_address = mac_address,
-            ip_address  = ip_address,
-            clk_freq    = sys_clk_freq)
+            ip_address  = convert_ip(ip_address),
+            clk_freq    = sys_clk_freq,
+            with_icmp   = True)
         self.submodules += ethphy, ethcore
         # timing constraints
+        ethphy.crg.cd_eth_rx.clk.attr.add("keep")
+        ethphy.crg.cd_eth_tx.clk.attr.add("keep")
         platform.add_period_constraint(ethphy.crg.cd_eth_rx.clk, 1e9/125e6)
-        platform.add_false_path_constraints(crg.cd_sys.clk, ethphy.crg.cd_eth_rx.clk)
+        platform.add_period_constraint(ethphy.crg.cd_eth_tx.clk, 1e9/125e6)
 
         # Led --------------------------------------------------------------------------------------
         counter = Signal(32)
@@ -65,7 +71,7 @@ class RGMIITest(SoCMini):
 
 def main():
     platform = colorlight_5a_75b.Platform(revision="7.0")
-    soc     = RGMIITest(platform)
+    soc     = RGMIITestSoC(platform)
     builder = Builder(soc, output_dir="build")
     builder.build()
 
